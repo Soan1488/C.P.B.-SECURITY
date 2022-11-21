29189f0344b5572830feedbc2848161bdc9c40c6
Your NameSoan1488 <andrisokolovskiy@gmail.com>
2022-11-21 21:32:37 +0100

    fixed mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,25 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
-  function toggleModal() {
-    refs.menu.classList.toggle('is-open');
+const refs = {
+  openMenuBtn: document.querySelector('[data-menu-open]'),
+  closeMenuBtn: document.querySelector('[data-menu-close]'),
+  menu: document.querySelector('[data-menu]'),
+  navList: document.querySelector('.mobile-nav__list'),
+};
+refs.openMenuBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.menu.classList.toggle('is-open');
+  if (refs.menu.classList.contains('is-open')) {
+    refs.navList.addEventListener('click', onNavListClick);
   }
-})();
+}
+
+function onNavListClick(e) {
+  if (e.target.classList.contains('mobile-nav__link')) {
+    closeMenu();
+  }
+}
+
+function closeMenu() {
+  refs.menu.classList.toggle('is-open');
+  refs.navList.removeEventListener('click', onNavListClick);
+}

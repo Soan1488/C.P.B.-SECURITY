a216219a37b6dd0665f7e1fbb553a3c6de42876c
Your NameSoan1488 <andrisokolovskiy@gmail.com>
2022-11-21 21:54:42 +0100

    add active and fixed modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,6 @@
 const refs = {
   openBtn: document.querySelector('[data-modal-open]'),
+  openBtnFooter: document.querySelector('[data-modal-open-footer]'),
   modalMenu: document.querySelector('[data-modal]'),
   closeBtn: document.querySelector('[data-modal-close]'),
   backdropRef: document.querySelector('.backdrop'),
@@ -7,14 +8,16 @@ const refs = {
 };
 
 refs.openBtn.addEventListener('click', onOpenBtnClick);
+refs.openBtnFooter.addEventListener('click', onOpenBtnClick);
 
 function onOpenBtnClick(e) {
   e.preventDefault();
+
+  refs.backdropRef.classList.remove('is-hidden');
+
   document.addEventListener('keydown', onEscTap);
   refs.backdropRef.addEventListener('click', onClickOutside);
   refs.closeBtn.addEventListener('click', onBtnClose);
-
-  refs.backdropRef.classList.remove('is-hidden');
   refs.form.addEventListener('submit', onSubmitBtnClick);
 
   document.body.style.overflow = 'hidden';
@@ -24,7 +27,7 @@ function onEscTap(e) {
   if (e.key === 'Escape') {
     refs.backdropRef.classList.add('is-hidden');
     document.body.style.overflow = '';
-    document.removeEventListener('keydown', onEscTap);
+    removeListeners();
   }
 }
 
@@ -32,19 +35,26 @@ function onClickOutside(e) {
   if (e.target.classList.value === 'backdrop') {
     refs.backdropRef.classList.add('is-hidden');
     document.body.style.overflow = '';
-    refs.backdropRef.removeEventListener('click', onClickOutside);
+    removeListeners();
   }
 }
 
 function onBtnClose(e) {
   refs.backdropRef.classList.add('is-hidden');
   document.body.style.overflow = '';
-  refs.closeBtn.removeEventListener('click', onBtnClose);
+  removeListeners();
 }
 
 function onSubmitBtnClick(e) {
   e.preventDefault();
   refs.form.reset();
   onBtnClose();
+  removeListeners();
+}
+
+function removeListeners() {
   refs.form.removeEventListener('submit', onSubmitBtnClick);
+  document.removeEventListener('keydown', onEscTap);
+  refs.closeBtn.removeEventListener('click', onBtnClose);
+  refs.backdropRef.removeEventListener('click', onClickOutside);
 }
